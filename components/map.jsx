b9be338ff22c5bc5906b0e2fcbe730b295a817b6
Jennifer Juan <jennifer.juan@spreeeed.com>
2022-04-18 20:55:23 +0800

    add marker back

--- components/map.jsx
+++ components/map.jsx
@@ -151,7 +151,7 @@ const Map = props => {
         layers={[
           ...baseLayers,
           region1, region2, 
-          // layerIconArt
+          layerIconArt
         ]}
         // effects={[lightingEffect]}
         getTooltip={({object}) => object && `${object.name}`}
